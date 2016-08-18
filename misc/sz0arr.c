f08567883c096f0a1f49e90abfd9176738b45816
aniljacob <tuxonnst@gmail.com>
2016-08-18 11:46:23 +0530

    int arr[0]. is this valid?

---
+++ misc/sz0arr.c
@@ -0,0 +1,31 @@
+/*
+In C, the members of a struct are always allocated in the order they appear. So, my_pkt->data is a pointer "one past the end" 
+of a pkt object. If initialized with
+
+my_pkt = malloc( sizeof( struct pkt ) + 50 );
+then my_pkt->data points to the beginning of a 50-byte general-purpose buffer.
+
+Only the final member of a struct is allowed to be defined this way.
+
+To be more compliant with C99, omit the 0 and write char data[];.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct mystruct{
+	int i;
+	char data[0];
+};
+
+int main(int argc, char *argv[])
+{
+	struct mystruct *mys_p50 = (struct mystruct *)malloc(sizeof(struct mystruct) + 50);
+
+	mys_p50->data[39] = 10;
+
+	printf("mys_p50 (%p) data pointer = %p, mys_p50->data[39] = %d\n", mys_p50, mys_p50->data, mys_p50->data[39]);
+	
+	return 0;
+}
